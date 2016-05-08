cc6a59fed670fcaccc3f580c92f8d29921d5ee3f
Jógvan Olsen <jo@jebster.net>
2016-05-08 15:42:48 +0200

    removed comments

--- app/routes/user.js
+++ app/routes/user.js
@@ -58,31 +58,11 @@ module.exports = function(app) {
         }
     });
 
-
-    // app.get('/api/user', function (req, res) {
-    //     User.find({}, function (err, users) {
-    //         res.send(users);
-    //     });
-    // });
-    //
-    // app.post('/api/user', function(req, res) {
-    //     var newUser = new User({
-    //         username: req.body.username,
-    //         password: req.body.password
-    //     });
-    //
-    //     newUser.save(function (err) {
-    //         if (!err) {
-    //             return res.status('200').send();
-    //         } else {
-    //             console.log(err);
-    //             if(err.name == 'ValidationError') {
-    //                 res.status(400).send({validation_error: err.errors});
-    //             } else {
-    //                 res.status(500).send({error: 'Server error'});
-    //             }
-    //         }
-    //     });
-    // });
+    // show user info
+    app.get('/api/user', function (req, res) {
+        User.find({}, function (err, users) {
+            res.send(users);
+        });
+    });
 
 };
